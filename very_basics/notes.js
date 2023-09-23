deef168ce8ae505841ccbf1f8d71aa5fa2d1c756
Protik Acharjay <protik7777777@gmail.com>
2023-09-23 21:32:39 +0600

    notes and basics

---
+++ very_basics/notes.js
@@ -0,0 +1,12 @@
+"use strict"; //use all the js code as newer version
+
+// typeof null is object
+
+// if the variable have "100abc" and it's converted using Number function, then it will return Nan value. which is number but (not a number)
+
+//null is identified as object & for conversion it will return 0
+
+let check= " "
+let boolcheck= Boolean(check)
+console.log(boolcheck);
+
